cf2dfefb9204cf60ee85094d9c409d5b9fa569a6
Kunal sharan <117339459+Kunal-sharan@users.noreply.github.com>
2024-07-09 22:28:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,23 +8,22 @@ from datetime import datetime, timedelta
 symbol = "^NSEBANK"
 st.header("Bank Nifty on Yahoo Finance : Standard deviation")
 # Define the date range for the last month
+end_date = datetime.now()
+start_date = end_date - timedelta(days=30)
 
+# Fetch historical data for Bank Nifty
+data = yf.download(symbol, start=start_date, end=end_date)
+
+# Extract closing and opening prices
+closing_prices = data['Close'].values
+opening_prices = data['Open'].values
+
+# Calculate standard deviation for closing prices
+std_dev_closing = np.std(closing_prices)
+# Calculate standard deviation for opening prices
+std_dev_opening = np.std(opening_prices)
 button=st.button("Calculate")
-if button:
-  end_date = datetime.now()
-  start_date = end_date - timedelta(days=30)
-
-  # Fetch historical data for Bank Nifty
-  data = yf.download(symbol, start=start_date, end=end_date)
-
-  # Extract closing and opening prices
-  closing_prices = data['Close'].values
-  opening_prices = data['Open'].values
-
-  # Calculate standard deviation for closing prices
-  std_dev_closing = np.std(closing_prices)
-  # Calculate standard deviation for opening prices
-  std_dev_opening = np.std(opening_prices)
+if button: 
   st.write(f"Standard Deviation of Closing Prices: {std_dev_closing}")
   st.write(f"Standard Deviation of Opening Prices: {std_dev_opening}")
   st.dataframe(data[['Open', 'Close']])
